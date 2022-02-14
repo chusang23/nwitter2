9692f2ccb456d7abefbe0a79f6d6398a5a862ebb
chusang23 <45816771+chusang23@users.noreply.github.com>
2022-02-14 17:41:44 +0900

    nwitter

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCnSMbZx5fBzJ9QNBu0haEVrkNJClZqbV0",
+    authDomain: "nwitter-6b26f.firebaseapp.com",
+    projectId: "nwitter-6b26f",
+    storageBucket: "nwitter-6b26f.appspot.com",
+    messagingSenderId: "272899405049",
+    appId: "1:272899405049:web:706d54be1bd025dd601e28"
+  };
